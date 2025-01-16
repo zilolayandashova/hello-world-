0f2e1d7367685a08718e824ba74e957f837cf7f3
z.yandashova <z.yandashova@newuu.uz>
2025-01-16 14:42:06 +0500

    age asking

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+  cout<<" Hello World"<<endl;
+  int age;
+  cout<<"Enter your age:"<<endl;
+  cin>>age;
+  cout<<"Your age:"<<age<<endl;18
+  return 0;
+}
